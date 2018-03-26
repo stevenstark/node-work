0dd51367ddcb055ea2c6cd21157c298260c43c39
stevenstark <stevenstark@gmail.com>
2018-03-25 21:34:01 -0700

    more improvement.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,22 +1,36 @@
-const exec = require('child_process').exec;
+const querystring = require ('querystring');
+const fs = require('fs');
 
-function start(response) {
+function start(response, postData) {
 	console.log('Request handler "start" was called.');
-
-	exec('find /', 
-		function (error, stdout, stderr) {
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.write(stdout);
+	 var body = '<html>'+
+		'<head>'+
+		'<meta http-equiv="Content-Type" '+
+		'content="text/html; charset=UTF-8" />'+
+		'</head>'+
+		'<body>'+
+		'<form action="/upload" method="post">'+
+		'<textarea name="text" rows="20" cols="60"></textarea>'+
+		'<input type="submit" value="Submit text" />'+
+		'</form>'+
+		'</body>'+
+		'</html>';
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write(body);
 		response.end();
-	});
 }
 
-function upload(response) {
+function upload(response, postData) {
 	console.log('Request handler "upload" was called.');
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write('hello from upload');
+	response.write('You have sent the text: ' + querystring.parse(postData).text);
 	response.end();
+}
+
+fuction show(response) {
+	content.log('Request handler "show" was called.');
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
